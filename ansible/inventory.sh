a7f49b5857a518e2e69e72fba8d1762ac1d9919b
Emil Koutanov <ekoutanov@gmail.com>
2017-09-29 17:07:09 +1000

    Support for single host, multiple port configurations

--- ansible/inventory.sh
+++ ansible/inventory.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Optional parameters
-if [ -z $DRIFTER_TARGET_BRANCH ]; then
+if [ -z "$DRIFTER_TARGET_BRANCH" ]; then
   DRIFTER_TARGET_BRANCH="master"
 fi
 
 # Mandatory parameters
-if [ -z $DRIFTER_HOSTS || \
-     -z $DRIFTER_TARGET_REPO ]; then
+if [ -z "$ANSIBLE_HOSTS" -o \
+     -z "$DRIFTER_TARGET_REPO" ]; then
   echo "One or more environment variables not set:"
-  echo "  DRIFTER_HOSTS: $DRIFTER_HOSTS"
+  echo "  ANSIBLE_HOSTS: $ANSIBLE_HOSTS"
   echo "  DRIFTER_TARGET_REPO: $DRIFTER_TARGET_REPO"
   echo "  DRIFTER_TARGET_BRANCH: $DRIFTER_TARGET_BRANCH"
   exit 1
@@ -21,37 +21,44 @@ drifter_home="~/.drifter"
 
 echo -e "{\n"
 
-id=1
-for dh in $DRIFTER_HOSTS
-do
-    dhs+="\"$dh\","
+id=0
+for dh in $ANSIBLE_HOSTS; do
+  df_host=`echo $dh | awk -F ":" '{print $1}'`
+  df_port=`echo $dh | awk -F ":" '{print $2}'`
+  if [ "$df_port" == "" ]; then
+    df_port="22"
+  fi
+
+  df_alias="df_${df_host}_${df_port}"
 
 cat << EOM
-    "drifter_$id": {
-        "hosts": [ "$dh" ],
-        "vars": {
-            "ansible_port": 2222,
-            "drifter_id": $id,
-            "drifter_host": "$dh"
-        }
-    },
+  "${df_alias}_group": {
+    "hosts": [ "$df_alias" ],
+    "vars": {
+      "drifter_id": $id,
+      "drifter_host": "$df_host",
+      "ansible_host": "$df_host",
+      "ansible_port": $df_port
+    }
+  },
 EOM
-    let id+=1
+  let id+=1
+  dhs+="\"$df_alias\","
 done
 
 cat << EOM
-    "drifter_hosts": {
-        "hosts": [ ${dhs%?} ],
-        "vars": {
-            "ansible_connection": "ssh",
-            "ansible_user": "$ANSIBLE_USER",
-            "ansible_ssh_private_key_file": "$ANSIBLE_SSH_PRIVATE_KEY_FILE",
-            "target_repo": "$DRIFTER_TARGET_REPO",
-            "target_branch": "$DRIFTER_TARGET_BRANCH",
-            "repo_name": "$repo_name",
-            "drifter_home": "$drifter_home"
-        }
+  "drifter_hosts": {
+    "hosts": [ ${dhs%?} ],
+    "vars": {
+      "ansible_connection": "ssh",
+      "ansible_user": "$ANSIBLE_USER",
+      "ansible_ssh_private_key_file": "$ANSIBLE_SSH_PRIVATE_KEY_FILE",
+      "target_repo": "$DRIFTER_TARGET_REPO",
+      "target_branch": "$DRIFTER_TARGET_BRANCH",
+      "repo_name": "$repo_name",
+      "drifter_home": "$drifter_home"
     }
+  }
 EOM
 
 echo -e "}\n"
