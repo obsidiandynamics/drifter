b1d7d9d0a5f30713cdf982a608f9bb7156723b53
Emil Koutanov <ekoutanov@gmail.com>
2017-11-20 08:13:18 +1100

    Minor refactoring

--- play.sh
+++ play.sh
@@ -7,7 +7,7 @@ RED='\033[0;31m'
 NC='\033[0m'
 echo_cmd="echo -e"
 $echo_cmd $CYAN
-./logo.sh
+./print-logo.sh
 $echo_cmd
 ./list-env.sh
 $echo_cmd $NC
@@ -22,6 +22,16 @@ while [ true ]; do
   exit_code=$?
   if [ "$exit_code" != 0 ]; then
     $echo_cmd "${RED}Ansible exited with code ${exit_code}.${NC}"
+    cd ..
+    node_dirs=`find out -mindepth 1 -maxdepth 1 -type d`
+    for node_dir in $node_dirs; do
+      if [ -f ${node_dir}/latest.tgz ]; then
+        latest_timestamp=`cat ${node_dir}/latest-timestamp`
+        target_file=${node_dir}/${latest_timestamp}.tgz
+        echo "Storing output in $target_file"
+        mv ${node_dir}/latest.tgz $target_file
+      fi
+    done
     exit 1;
   fi
 done
