39c39e701f1c948d31cc6a56569ce41b2f830df0
Emil Koutanov <ekoutanov@gmail.com>
2017-11-20 10:58:15 +1100

    Optional loop output logging

--- loop.sh
+++ loop.sh
@@ -6,6 +6,10 @@ if [ $# -lt 3 ]; then
   exit 1
 fi
 
+if [ "$DRIFTER_LOOP_LOG" == "true" ]; then
+  echo "Gradle output logging is enabled"
+fi
+
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 YELLOW='\033[0;33m'
@@ -33,20 +37,29 @@ start_time=`date +%s`
 
 while [ true ]; do
   date=`date +%F-%H%M%S`
-  ./gradlew cleanTest $params
-  exit_code=$?
+  log_file=/tmp/loop-${start_time}.log
+  if [ "$DRIFTER_LOOP_LOG" == "true" ]; then
+    ./gradlew cleanTest $params 2>&1 | tee $log_file
+  else
+    ./gradlew cleanTest $params
+  fi
+  exit_code=${PIPESTATUS[0]}
   if [ "${exit_code}" == "1" ]; then
-    target_date=${target_base}/${date}
+    target_date_dir=${target_base}/${date}
     build_dirs=`find . -type d | grep /build/reports\$`
     for build_dir in $build_dirs; do
       module_dir=$(dirname "$(dirname "$build_dir")")
       module_name=`echo $module_dir | awk -F "/" '{print $NF}'`
-      target_dir=${target_date}/${module_name}
+      target_dir=${target_date_dir}/${module_name}
       mkdir -p $target_dir
       echo -e "${GREY}Copying $build_dir -> $target_dir ${NC}"
       cp -r $build_dir $target_dir
     done
-    echo -e "${RED}Gradle process completed with code ${exit_code};${NC} saved test results in ${target_date}"
+
+    if [ "$DRIFTER_LOOP_LOG" == "true" ]; then
+      mv $log_file ${target_date_dir}/loop.log
+    fi
+    echo -e "${RED}Gradle process completed with code ${exit_code};${NC} saved test results in ${target_date_dir}"
     echo $date > ${target_base}/latest-timestamp
     exit 1
   elif [ "${exit_code}" == "126" -o "${exit_code}" == "127" ]; then  
