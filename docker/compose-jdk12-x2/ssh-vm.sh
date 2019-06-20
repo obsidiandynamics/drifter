b90e93de269934c8bab1fdda14fef3764d3e9564
Emil Koutanov <ekoutanov@gmail.com>
2019-06-20 20:43:37 +1000

    Updated Dockerfile

--- docker/compose-jdk12-x2/ssh-vm.sh
+++ docker/compose-jdk12-x2/ssh-vm.sh
@@ -7,5 +7,4 @@ fi
 
 vm_index=$1
 port=$(($vm_index + 2200))
-#ssh root@localhost -p $port -t -i ~/.vagrant.d/insecure_private_key "export PS1=\"root@vm${vm_index}# \" && bash"
 ssh root@localhost -p $port -t -i ~/.vagrant.d/insecure_private_key "export PS1='\[\e[1;32m\]\u@vm${vm_index}~ssh# ' && bash"
