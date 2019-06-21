0f360b92b711274ad7764e2b363d7d8f75d1768c
Emil Koutanov <ekoutanov@gmail.com>
2019-06-21 12:48:11 +1000

    Fixed image

--- docker/image-jdk12/README.sh
+++ docker/image-jdk12/README.sh
@@ -9,7 +9,7 @@ build.sh
 # Running
 Start the container:
 ```sh
-docker run -it --rm --name drifter-jdk12 -p 22:22 obsidiandynamics/drifter-jdk12
+docker run -it --rm --name drifter-jdk12 -p 22:22 -e DIND_HOST=localhost obsidiandynamics/drifter-jdk12
 ```
 
 Connect to the image:
